a7b03e45756307feb9b7a8abc819e56dad4ad284
VimothSusara <116062952+VimothSusara@users.noreply.github.com>
2023-03-22 14:37:06 +0530

    logo size change

--- sdgp/lib/screens/phoneauth_screen.dart
+++ sdgp/lib/screens/phoneauth_screen.dart
@@ -221,8 +221,8 @@ class _PhoneAuthScreenState extends State<PhoneAuthScreen> {
                   // const Spacer(),
                   Image(
                     image: AssetImage(Config.bottom_logo),
-                    height: 150,
-                    width: 300,
+                    height: 50,
+                    width: 150,
                     fit: BoxFit.cover,
                   ),
                 ],
